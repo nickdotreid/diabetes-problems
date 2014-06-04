e6cf8ea454bb74b64d91a69e7f3b9673fafc0b14
Nick Reid <nickreid@nickreid.com>
2014-06-04 18:41:20 -0400

    added sessions to models

--- problems/models.py
+++ problems/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.db.models.signals import pre_save, post_save, pre_delete
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Problem(models.Model):
     class Meta:
@@ -11,4 +14,30 @@ class Problem(models.Model):
 
     def __unicode__(self):
         return self.title
+
+class Session(models.Model):
+    class Meta:
+        verbose_name = 'Session'
+        verbose_name_plural = 'Sessions'
+
+    key = models.CharField( unique=True, max_length=30)
+    user = models.ForeignKey(User, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.key
+        
+def session_make_key():
+	key = User.objects.make_random_password(30)  #make a random string the size of key field
+	try:
+		Session.objects.get(key=key) # if this key is in use, recurse
+		return session_make_key()
+	except:
+		return key
+
+def session_create_key(sender, instance, **kwargs):
+	if 'raw' in kwargs and kwargs['raw']:
+		return
+	if not instance.key:
+		instance.key = User.objects.make_random_password(30)
+pre_save.connect(session_create_key, sender=Session)
     
