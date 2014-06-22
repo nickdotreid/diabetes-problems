f5cd0924e2ac6327a9ebdb705ed809ffeb3d9784
Nick Reid <nickreid@nickreid.com>
2014-06-22 13:04:19 -0400

    added updating selected count

--- diabetes_problems/static/hdbus/js/main.js
+++ diabetes_problems/static/hdbus/js/main.js
@@ -6,4 +6,19 @@ $(document).ready(function(){
 			$(this).parents('.problem').removeClass("problem-selected");
 		}
 	});
+
+	$('.form-actions .container').prepend('<div class="selected-count"></div>');
+	var selected_count_update = function(){
+		var selected = $('form .problem input:checked').length;
+		var total =  $('form .problem').length;
+		var update_div = $('.form-actions .selected-count');
+		if(selected==0){
+			update_div.html("Please select a problem");
+		}else{
+			update_div.html(selected+" of "+total+" problems");
+		}
+	}
+	$('form').delegate('.problem input','click',selected_count_update);
+	selected_count_update();
+
 });
