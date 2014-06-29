04b01f5c227327976cd3de38739bb0e9453691cb
Nick Reid <nickreid@nickreid.com>
2014-06-29 17:59:40 -0400

    switched around product URLS

--- problems/urls.py
+++ problems/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('problems.views',
-	url(r'^(?P<session_key>\w+)/thanks','thanks'),
-	url(r'^(?P<session_key>\w+)','important'),
+	url(r'^(?P<session_key>\w+)/problems','important'),
+	url(r'^(?P<session_key>\w+)','thanks'),
 	url(r'^','important'),
 )
