b49eb7c6e926bd897aa4a1a9d0aec0d4669f7172
Nick Reid <nickreid@nickreid.com>
2014-07-06 11:56:37 -0400

    changed update message

--- diabetes_problems/static/hdbus/js/main.js
+++ diabetes_problems/static/hdbus/js/main.js
@@ -12,12 +12,8 @@ $(document).ready(function(){
 		var selected = $('form .problem input:checked').length;
 		var total =  $('form .problem').length;
 		var update_div = $('.form-actions .selected-count');
-		if(selected==0){
-			update_div.html("Please select a problem");
-		}else{
-			update_div.html(selected+" of "+total+" problems");
-		}
-	}
+		update_div.html(selected+" of "+total+" problems");
+	};
 	$('form').delegate('.problem input','click',selected_count_update);
 	selected_count_update();
 
