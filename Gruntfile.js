ca616f12651a2f8222e52587d2503e5bd1d49051
Nick Reid <nickreid@nickreid.com>
2014-08-20 17:42:28 -0400

    added diabetes problems

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "diabetes_problems/static/hdbus/css/styles.css": "diabetes_problems/static/hdbus/less/styles.less"
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['diabetes_problems/static/hdbus/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+};
