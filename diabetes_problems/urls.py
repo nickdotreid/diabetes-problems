a95484ae9c2250879511e5468350e79cd63144a5
Nick Reid <nickreid@nickreid.com>
2014-08-20 23:33:55 -0400

    added ajax catch back

--- diabetes_problems/urls.py
+++ diabetes_problems/urls.py
@@ -8,12 +8,11 @@ add_to_builtins('athumb.templatetags.thumbnail')
 admin.autodiscover()
 
 urlpatterns = patterns("",
-
     # Admin URLs.
     url(r"^admin/", include(admin.site.urls)),
     # There's no favicon here!
     url(r"^favicon.ico$", generic.RedirectView.as_view()),
     url(r"^issues/", include('problems.urls')),
-    url(r"^", include('main.urls')),
     url(r"^$", 'ajax.views.page'),
+    url(r"^", include('main.urls')),
 )
